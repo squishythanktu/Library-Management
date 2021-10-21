d6c11aa56ebc2fa8e8397b6234067be4601b3da3
squishythanktu <lethanhtu164@gmail.com>
2021-10-21 10:19:14 +0700

    Update password and sub menu

---
+++ Nguoiquanly.h
@@ -0,0 +1,16 @@
+#ifndef NGUOIQUANLY_H
+#define NGUOIQUANLY_H
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fstream>
+#include<string.h>
+#include<conio.h>
+#include<Windows.h>
+using namespace std;
+
+class Nguoiquanly{
+
+};
+
+#endif
---
+++ Sinhvien.h
@@ -0,0 +1,19 @@
+#ifndef SINHVIEN_H
+#define SINHVIEN_H
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fstream>
+#include<string.h>
+#include<conio.h>
+#include<Windows.h>
+using namespace std;
+
+class Sinhvien{
+        private:
+                string msv;
+        public:
+                
+};
+
+#endif
